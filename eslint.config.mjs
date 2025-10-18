7845f62f23fdf48c965df5504ffb91b0d5d4b039
Dustin Griffith <dustingriffith@outlook.com>
2025-10-18 18:34:39 -0400

    init checkin

---
+++ eslint.config.mjs
@@ -0,0 +1,80 @@
+import css from '@eslint/css';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import pluginReact from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactCompiler.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [{ regex: '^@mui/[^/]+$' }],
+        },
+      ],
+      'no-unnecessary-use-callback': 'error',
+      'react-compiler/react-compiler': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,ts,jsx,tsx}'],
+    plugins: { pluginReact },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: ['plugin:react/jsx-runtime'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [{ regex: '^@mui/[^/]+$' }],
+        },
+      ],
+      'no-unnecessary-use-callback': 'error',
+      'react-compiler/react-compiler': 'error',
+    },
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+]);
